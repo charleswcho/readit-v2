4809df1a95d81591b783545ddfb7ae938b328449
Charles Wesley Cho <charleswcho@gmail.com>
2019-04-14 09:37:48 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 import './App.css';
 
+const styles = theme => ({
+  appBar: {
+    top: 'auto',
+    bottom: 0
+  },
+  toolbar: {
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  }
+});
+
 class App extends Component {
+  state = {
+    value: 0
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">Readit</Typography>
+          </Toolbar>
+        </AppBar>
+
+        <Typography component="h2" variant="h1" gutterBottom>
+          Hi there
+        </Typography>
+
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Tabs centered value={value} onChange={this.handleChange}>
+            <Tab label="Top" />
+            <Tab label="Popular" />
+            <Tab label="Hot" />
+          </Tabs>
+        </AppBar>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#0080ff'
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    divider: '#1d2731',
+    background: '#1d2731'
+  },
+  typography: { useNextVariants: true }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <App />
+  </MuiThemeProvider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
