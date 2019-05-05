465265763d1aa9a64f7f0656c01c004ea24aef7c
Charles Wesley Cho <charleswcho@gmail.com>
2019-05-05 16:16:14 -0700

    Updates

---
+++ precache-manifest.5bdc60d681d41444f492ccf9288fbc55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "69ddc4263b73078a72e8",
+    "url": "/readit-v2/static/css/main.96d5413e.chunk.css"
+  },
+  {
+    "revision": "69ddc4263b73078a72e8",
+    "url": "/readit-v2/static/js/main.fc04dec6.chunk.js"
+  },
+  {
+    "revision": "2fe4e2fe8ddae74cb414",
+    "url": "/readit-v2/static/js/runtime~main.91ee3ea3.js"
+  },
+  {
+    "revision": "8c953958cb2d272d8d9f",
+    "url": "/readit-v2/static/js/2.e9c86f8a.chunk.js"
+  },
+  {
+    "revision": "da834345ee72e20519d5a4806b92ac7a",
+    "url": "/readit-v2/index.html"
+  }
+];
